fd8075a5e163640ca637c209228a82af688c2ade
Manuel <160016796+Manuel-agd@users.noreply.github.com>
2024-09-03 11:50:21 +0200

    Main.ts limpiado

---
+++ main.js
@@ -0,0 +1,42 @@
+var estiloartista = "color: green; font-size: 20px; font-weigth: bold";
+;
+var grupo1 = {
+    artista: "The Beatles",
+    fundacion: 1960,
+    activo: true,
+    genero: "Pop Rock",
+};
+console.log("%cThe Beatles", estiloartista);
+console.log(grupo1);
+var grupo2 = {
+    artista: "Queen",
+    fundacion: 1970,
+    activo: false,
+    genero: "Rock",
+};
+console.log("%cQueen", estiloartista);
+console.log(grupo2);
+var grupo3 = {
+    artista: "ACDC",
+    fundacion: 1973,
+    activo: true,
+    genero: "Hard Rock",
+};
+console.log("%cACDC", estiloartista);
+console.log(grupo3);
+var grupo4 = {
+    artista: "Ludwig Van Beethoven",
+    fundacion: 1770,
+    activo: false,
+    genero: "Clasica",
+};
+console.log("%cLudwig Van Beethoven", estiloartista);
+console.log(grupo4);
+var grupo5 = {
+    artista: "The Rolling Stones",
+    fundacion: 1962,
+    activo: true,
+    genero: "Rock",
+};
+console.log("%cThe Rolling Stones", estiloartista);
+console.log(grupo5);
